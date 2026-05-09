80df061269f581e2982ee803a9231f8e43ad2c41
Collinsodiobi <Collinsodi10@gmail.com>
2026-05-09 20:10:16 +0200

    Fix pow function with negative exponent support

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def pow(a, b):
+    if b == 0:
+        return 1
+
     result = 1
-    for i in range(b):
+
+    if b > 0:
+        for i in range(b):
+            result *= a
+        return result
+
+    # negative exponent
+    for i in range(-b):
         result *= a
-    return result
+    return 1 / result
