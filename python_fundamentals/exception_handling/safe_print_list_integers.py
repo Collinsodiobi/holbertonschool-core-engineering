f1d0f99d9ad9aee6976d393569fe46d95d36bb5c
Collinsodiobi <Collinsodi10@gmail.com>
2026-05-11 12:39:56 +0200

    Fix safe print list integers task

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -6,9 +6,9 @@ def safe_print_list_integers(my_list=[], x=0):
 
     for i in range(x):
         try:
-            value = my_list[i]
-            if isinstance(value, int):
-                print("{:d}".format(value), end="")
+            item = my_list[i]
+            if isinstance(item, int):
+                print("{:d}".format(item), end="")
                 count += 1
         except IndexError:
             break
