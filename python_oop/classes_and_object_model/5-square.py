f6307739bf3fac63398ccc758b54f7b277f1a6e1
Colinsodiobi <collinsodi10@gmail.com>
2026-05-13 22:25:07 +0200

    Add my_print method to Square

---
+++ python_oop/classes_and_object_model/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        """Returns the area of the square."""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Gets the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def my_print(self):
+        """Prints the square using '#' characters."""
+        if self.__size == 0:
+            print("")
+            return
+
+        for _ in range(self.__size):
+            print("#" * self.__size)
