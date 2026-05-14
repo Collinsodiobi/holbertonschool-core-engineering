fd5fe5b7f185cd5032f925057c8a8e0b834d4da2
Collinsodiobi <Collinsodi10@gmail.com>
2026-05-14 13:25:11 +0200

    Fix Square init to pass size as width and height

--- python_oop/inheritance/2-square.py
+++ python_oop/inheritance/2-square.py
@@ -7,6 +7,11 @@ Rectangle = __import__('2-rectangle').Rectangle
 class Square(Rectangle):
     """Square class inheriting from Rectangle."""
 
+    def __init__(self, size):
+        """Initialize square with size."""
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+
     def __str__(self):
         """Return square description."""
         return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
