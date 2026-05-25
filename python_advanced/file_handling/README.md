93f1b0a4939db65846286358ec24bfe5b94c8c7a
Collinsodiobi <Collinsodi10@gmail.com>
2026-05-25 11:29:01 +0200

    Task 0: read file implementation

---
+++ python_advanced/file_handling/README.md
@@ -0,0 +1,3 @@
+# File Handling in Python
+
+This project contains exercises about reading files in Python using open(), with statement, and UTF-8 encoding.
---
+++ python_advanced/file_handling/read_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+def read_file(filename=""):
+    """Reads a UTF8 text file and prints it to stdout"""
+
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
